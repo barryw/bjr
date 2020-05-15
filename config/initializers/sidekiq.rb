018f6cbdb3e8a5348b59d9e2d03c13f263dfb564
Barry Walker <barrywalker@gmail.com>
2020-05-15 07:51:49 -0400

    Change sidekiq polling interval to try and make scheduling more responsive

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Configure Sidekiq's Redis
 Sidekiq.configure_server do |config|
   config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
   config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', 6379)}/#{ENV.fetch('REDIS_SIDEKIQ_DB', 0)}" }
@@ -9,6 +10,9 @@ Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{ENV.fetch('REDIS_HOST', 'localhost')}:#{ENV.fetch('REDIS_PORT', 6379)}/#{ENV.fetch('REDIS_SIDEKIQ_DB', 0)}" }
 end
 
+# Load up system jobs
 schedule_file = "#{Rails.root}/config/schedule.yml"
-
 Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
+
+# Look for schedulables every 10 seconds
+Sidekiq.options[:poll_interval] = 10
